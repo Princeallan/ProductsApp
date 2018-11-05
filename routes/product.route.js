c2caa4e5740d1fe2d0e7c6497b047802eda653f3
princeallan <allankirui57@gmail.com>
2018-11-05 12:02:05 +0300

    router created

---
+++ routes/product.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = Router();
+
+const product_controller = require('../controllers/product.controller');
+
+// a simple test url to check that all of our files are communicating correctly
+router.get('/test', product_controller.test);
+
+module.exports = router;
